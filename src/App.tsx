3c32cd129b05bd9f0216369f7b3e6ed8e06c5943
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-07-03 20:07:18 +0300

    add selects

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ interface Coin {
     circulating_supply: string;
 }
 
-const getCoins = async () => {
-    const res = await fetch(`${API_URL}?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`);
+const getCoins = async (coinsParams: string) => {
+    const res = await fetch(`${API_URL}?${coinsParams}&sparkline=false`);
 
     if (!res.ok) {
         throw new Error('Failed to fetch data');
@@ -21,40 +21,69 @@ const getCoins = async () => {
 };
 
 export const App = () => {
-    const [coins, setCoins] = useState<Coin[]>([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [coins, setCoins] = useState<Coin[]>([]);
+    const [currentPriceValue, setCurrentPriceValue] = useState('usd');
+    const [orderValue, setOrderValue] = useState('market_cap_desc');
+    const [perPageValue, setPerPageValue] = useState('10');
 
     useEffect(() => {
         setIsLoading(true);
 
-        getCoins()
+        getCoins(`vs_currency=${currentPriceValue}&order=${orderValue}&per_page=${perPageValue}&page=1`)
             .then(setCoins)
             .finally(() => setIsLoading(false));
-    }, []);
+    }, [currentPriceValue, orderValue, perPageValue]);
 
     return (
         <section>
             <h1>Coins & Markets</h1>
 
             {!isLoading ? (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Current Price</th>
-                            <th>Circulating Supply</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {coins.map(({ id, name, current_price, circulating_supply }) => (
-                            <tr key={id}>
-                                <td>{name}</td>
-                                <td>{current_price}</td>
-                                <td>{circulating_supply}</td>
+                <>
+                    <div>
+                        <select value={currentPriceValue} onChange={({ target }) => setCurrentPriceValue(target.value)}>
+                            <option value='usd'>USD</option>
+                            <option value='eur'>EUR</option>
+                        </select>
+
+                        <select value={orderValue} onChange={({ target }) => setOrderValue(target.value)}>
+                            <option value='market_cap_desc'>Market cap descending</option>
+                            <option value='market_cap_asc'>Market cap ascending</option>
+                        </select>
+                    </div>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Current Price</th>
+                                <th>Circulating Supply</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {coins.map(({ id, name, current_price, circulating_supply }) => (
+                                <tr key={id}>
+                                    <td>{name}</td>
+                                    <td>
+                                        {current_price} {currentPriceValue}
+                                    </td>
+                                    <td>{circulating_supply}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
+                    <div>
+                        <select value={perPageValue} onChange={({ target }) => setPerPageValue(target.value)}>
+                            <option value='5'>5 / page</option>
+                            <option value='10'>10 / page</option>
+                            <option value='20'>20 / page</option>
+                            <option value='50'>50 / page</option>
+                            <option value='100'>100 / page</option>
+                        </select>
+                    </div>
+                </>
             ) : (
                 <div>
                     <span>Loading...</span>
